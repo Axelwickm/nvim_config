aa3724744388c266d3ee3a1a6ccada1067103689
Axel Wickman <axelwickm@gmail.com>
2023-08-26 20:56:12 +0100

    Copy to clipboard in visual mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,4 @@
 -- Add any additional keymaps here
 
 vim.keymap.set("n", "<leader>y", '"+y', { noremap = true, silent = true })
+vim.keymap.set("x", "<leader>y", '"+y', { noremap = true, silent = true })
