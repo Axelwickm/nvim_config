7c3aea5b9a2d36ddfc3add339533969ee17c7e2c
Axel Wickman <axelwickm@gmail.com>
2023-08-26 20:07:30 +0100

    Editor functionality

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -24,6 +24,13 @@ return {
         end,
         desc = "Resume telescope",
       },
+      {
+        "<leader>fp",
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
+        desc = "Find Plugin File",
+      },
     },
     -- change some options
     opts = {},
